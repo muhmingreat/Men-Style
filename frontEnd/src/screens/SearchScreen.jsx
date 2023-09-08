529909aaaefdc5a5d7f03d5d0088a87d9f259ce2
muhmingreat <ayatullahmumin3@gmail.com>
2023-09-08 07:41:54 +0100

    update

--- frontEnd/src/screens/SearchScreen.jsx
+++ frontEnd/src/screens/SearchScreen.jsx
@@ -77,9 +77,9 @@ export default function SearchScreen() {
   const sp = new URLSearchParams(search); // /search?category=Shirts
   const category = sp.get('category') || 'all';
   const query = sp.get('query') || 'all';
-  const price = sp.get('price') || 'all';
-  const rating = sp.get('rating') || 'all';
-  const order = sp.get('order') || 'newest';
+  const [price] = sp.get('price') || 'all';
+  const [rating] = sp.get('rating') || 'all';
+  const [order] = sp.get('order') || 'newest';
   const page = sp.get('page') || 1;
 
   const [{ loading, error, products, pages, countProducts }, dispatch] =
